1b214f27ddaf8c08df38dc1f023586d8f08e3f6f
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-01-14 13:18:55 +0800

    feat: update page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Auth PKCE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
